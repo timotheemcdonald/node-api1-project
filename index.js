9b887235eeb5456b46a2162e62bcc1a170d04aa6
timotheemcdonald <timotheemcdonald@gmail.com>
2020-08-31 17:34:15 -0400

    almost done

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const server = express()
+const shortid = require('shortid')
+
+server.use(express.json())
+
+let resource = [
+    {
+        id: shortid.generate(), // hint: use the shortid npm package to generate it
+        name: "Jane Doe", // String, required
+        bio: "Not Tarzan's Wife, another Jane",  // String, required
+      }
+]
+
+server.get("/api/users", (req, res) => {
+    res.status(200).json({data: resource})
+})
+
+server.get("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id)
+    let found = resource.find(data => data.id === id)
+    
+    if (found){
+        res.status(200).json({data: resource.id})
+    } else {
+        res.status(404).json({message: "The user with the specified ID does not exist." })
+    }
+})
+
+server.post("/api/users", (req,res) => {
+    const newResource = req.body
+    const newName = newResource.name
+    const newBio = newResource.bio
+    // resource.push(newResource)
+    // res.status(201).json({data: resource})
+    if(newName === "" || newBio === "" ){
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user."})
+    }else{
+        resource.push(newResource)
+        res.status(201).json({data: resource})
+    }
+})
+
+server.put("/api/users/:id", (req,res) => {
+    const id = Number(req.params.id)
+    const change = req.body
+    const newName = change.name
+    const newBio = change.bio
+    let found = resource.find(data => data.id === id)
+
+    if (found){
+        Object.assign(found, change)
+        res.status(200).json(found)
+    } else {
+        res.status(404).json({message: "The user with the specified ID does not exist." })
+    }
+
+    if(newName === "" || newBio === "" ){
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user."})}
+})
+
+server.delete("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id)
+    resource = resouce.filter(data => data.id !== id)
+    let found = resource.find(data => data.id === id)
+
+    if(!found){
+        res.status(404).json({message: "The user with the specified ID does not exist." })
+    }
+
+    res.status(200).json(resource)
+})
+
+const port = 9000
+
+server.listen(port, () => console.log('server is up on 9000'))
